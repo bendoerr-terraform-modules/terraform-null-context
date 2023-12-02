0a287ab5139ae232391c9da0ca570d02bd838358
Ben Doerr <craftsman@bendoerr.me>
2023-12-01 21:58:20 -0500

    📝 (docs): Improves and cleans the documentation (#28)

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "namespace" {
     Element to ensure resources are generated with names that are globally
     unique and do not collide. This should be a short key such as initials.
     EOT
+  nullable    = false
 }
 
 variable "environment" {
@@ -13,6 +14,7 @@ variable "environment" {
     Element to identify the region and/or the role. If not provided this element
     defaults to <role_short>-<region_short>(-<instance_short>).
     EOT
+  nullable    = true
 }
 
 variable "role" {
@@ -23,6 +25,7 @@ variable "role" {
     tags to ensure that identification is simple across accounts. Examples
     'production', 'development', 'main'.
     EOT
+  nullable    = false
 }
 
 variable "role_short" {
@@ -33,6 +36,7 @@ variable "role_short" {
     Automatic shortening is done by removal of vowels unless handled by special
     cases such as 'production' => 'prod', or 'development' => 'dev'.
     EOT
+  nullable    = true
 }
 
 variable "region" {
@@ -41,6 +45,7 @@ variable "region" {
   description = <<-EOT
     Key for the hosting provider region.
     EOT
+  nullable    = false
 }
 
 variable "region_short" {
@@ -51,6 +56,7 @@ variable "region_short" {
     Automatic shortening is done by removal of vowels unless handled by special
     cases such as 'us-east-1' => 'ue1', or 'us-west-2' => 'uw2'.
     EOT
+  nullable    = true
 }
 
 variable "instance" {
@@ -60,6 +66,7 @@ variable "instance" {
     Element to identify a tenant or copy of an environment (blue-green
     deployments). This is not used often.
     EOT
+  nullable    = true
 }
 
 variable "instance_short" {
@@ -69,26 +76,30 @@ variable "instance_short" {
     Shortened version of the 'instance'.
     Automatic shortening is done by removal of vowels.
     EOT
+  nullable    = true
 }
 
 variable "project" {
-  type = string
-  default = ""
+  type        = string
+  default     = ""
   description = <<-EOT
     Unique identifier for this project
     EOT
+  nullable    = false
 }
 
 variable "attributes" {
   type        = list(string)
   default     = []
   description = "Additional id elements that would be appended."
+  nullable    = true
 }
 
 variable "tags" {
   type        = map(string)
   default     = {}
   description = "Additional tags to include."
+  nullable    = true
 }
 
 variable "context" {
@@ -108,4 +119,5 @@ variable "context" {
     tags           = {}
   }
   description = "Allows the merging of an existing context with this one."
+  nullable    = true
 }
