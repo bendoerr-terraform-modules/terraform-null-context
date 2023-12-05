bfaab58150dfc1f7e0525a46059bb73c8f3e4ee5
Ben Doerr <craftsman@bendoerr.me>
2023-12-04 22:31:25 -0500

    🚨 (lint): Adds and runs pre-commit linters (#34)

--- examples/complete/ctx_no_env_no_instance.tf
+++ examples/complete/ctx_no_env_no_instance.tf
@@ -1,12 +1,12 @@
 module "ctx_no_env_no_instance" {
-  source         = "../.."
-  namespace      = var.namespace
-  role           = var.role
-  role_short     = var.role_short
-  region         = var.region
-  region_short   = var.region_short
-  attributes     = var.attributes
-  tags           = var.tags
+  source       = "../.."
+  namespace    = var.namespace
+  role         = var.role
+  role_short   = var.role_short
+  region       = var.region
+  region_short = var.region_short
+  attributes   = var.attributes
+  tags         = var.tags
 }
 
 output "ctx_no_env_no_instance_shared" {
--- examples/complete/ctx_short.tf
+++ examples/complete/ctx_short.tf
@@ -1,10 +1,10 @@
 module "ctx_short" {
-  source         = "../.."
-  namespace      = var.namespace
-  role           = var.role
-  region         = var.region
-  attributes     = var.attributes
-  tags           = var.tags
+  source     = "../.."
+  namespace  = var.namespace
+  role       = var.role
+  region     = var.region
+  attributes = var.attributes
+  tags       = var.tags
 }
 
 output "ctx_short_shared" {
